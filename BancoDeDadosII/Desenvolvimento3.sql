4482cb84f83d007df110c0934e2c6ad41d4ac7c0
Tiago Marinho <125687146+tmarinhorj@users.noreply.github.com>
2023-12-26 12:14:50 -0300

    Update Desenvolvimento3.sql

--- BancoDeDadosII/Desenvolvimento3.sql
+++ BancoDeDadosII/Desenvolvimento3.sql
@@ -1,1 +1,9 @@
+DELIMITER $$
+CREATE PROCEDURE RelatorioDiarioCompras()
+BEGIN
+  SELECT data_compra, id_produto, COUNT(*) as quantidade
+  FROM compras
+  GROUP BY data_compra, id_produto;
+END $$
+DELIMITER ;
 
